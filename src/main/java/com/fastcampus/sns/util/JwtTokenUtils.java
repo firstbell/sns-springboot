d29a3b6e72d848b1eeb87cee3683117f8841412a
Jonathan <jonnygim.dev@gmail.com>
2022-12-29 15:49:25 +0900

    feat : 토큰 생성을 위한 util 추가

---
+++ src/main/java/com/fastcampus/sns/util/JwtTokenUtils.java
@@ -0,0 +1,31 @@
+package com.fastcampus.sns.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtTokenUtils {
+
+    // util 이기 때문에 static 으로 선언해 줌
+    public static String generateToken(String userName, String key, long expiredTimeMs) {
+        Claims claims = Jwts.claims();
+        claims.put("userName", userName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredTimeMs))
+                .signWith(getKey(key), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private static Key getKey(String key) {
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
